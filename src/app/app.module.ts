5de27d02e74a3de40a7553f82aa7ec2db8cb9c9a
Sakthivel Anandhan <shakthivelanandhan@gmail.com>
2023-07-08 23:24:35 +0530

    initiates basic API health check from web application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppService } from './services/app.service';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
-    providers: [],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [AppService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
